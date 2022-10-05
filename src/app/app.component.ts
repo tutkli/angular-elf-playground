e82b16a1c07c948256463f9c3b227b1ef53ebf14
Clara Castillo <claracastillochale@gmail.com>
2022-10-05 21:03:17 +0200

    Make project standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'angular-elf-playground';
